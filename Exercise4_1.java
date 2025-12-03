5265bb48d754d72a3ec1a6100db78bb98e604e9c
240541125-jpg <240541125@firat.edu.tr>
2025-12-03 18:39:38 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,28 @@
+
+import java.util.Scanner;
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day+","+ month + ""+ date + ","+ year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day+","+ date +""+ month +","+ year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        Scanner in = new Scanner(System.in);
+        System.out.print("Gunu giriniz(Monday): ");
+        String day = in.nextLine();
+        System.out.print("Ayi giriniz(November): ");
+        String month = in.nextLine();
+        System.out.print("Tarihi giriniz(23): ");
+        int date = in.nextInt();
+        System.out.print("Yili giriniz(1978): ");
+        int year = in.nextInt();
+        System.out.println("\nAmerican format:");
+        printAmerican(day, date, month, year);
+        System.out.println("\nEuropean format:");
+        printEuropean(day, date, month, year);
     }
 }
