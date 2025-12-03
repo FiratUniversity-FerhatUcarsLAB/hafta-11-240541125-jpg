bfe486b450906ea2fc412c882be3bbe21f76f08c
240541125-jpg <240541125@firat.edu.tr>
2025-12-03 18:51:26 +0300

    Add execution order comments in Exercise4_2.java
    Added comments to clarify the execution order of the program.

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -3,21 +3,21 @@ public class Exercise4_2 {
   // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        zippo("rattle", 13); //1) Program buradan başlar.
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println(blimp); //4) "rattle" yazdırılır. 
+        zippo("ping", -5); //5) zippo tekrar çağrılır.
     }
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+        if (flag < 0) {  //6) -5<0 olduğu için TRUE
+            System.out.println(quince + " zoop"); //7) "ping zoop"
+        } else {  //2) 13<0 FALSE olduğu için else çalışır.
+            System.out.println("ik"); // 3) "ik" yazdırılır.
+            baffle(quince);  // 4) baffle ("raffle") çalışır.
+            System.out.println("boo-wa-ha-ha"); // 8) Son olarak bu yazılır.
         }
     }
 }
